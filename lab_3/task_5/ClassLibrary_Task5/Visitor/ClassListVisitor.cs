2fe67e5c1681e385db98e5a8eaa4077df56f2571
Мельник Володимир Вікторович <ipz235_mvv@student.ztu.edu.ua>
2025-05-15 10:37:34 +0300

    Visitor

---
+++ lab_3/task_5/ClassLibrary_Task5/Visitor/ClassListVisitor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace ClassLibrary_Task5.Visitor
+{
+    public class ClassListVisitor : IVisitor
+    {
+        public HashSet<string> Classes { get; } = new HashSet<string>();
+
+        public void VisitElement(LightElementNode element)
+        {
+            foreach (var className in element.CssClasses)
+                Classes.Add(className);
+
+            foreach (var child in element.Children)
+                child.Accept(this);
+        }
+
+        public void VisitText(LightTextNode text) { }
+    }
+}
---
+++ lab_3/task_5/ClassLibrary_Task5/Visitor/IVisitor.cs
@@ -0,0 +1,8 @@
+﻿namespace ClassLibrary_Task5.Visitor
+{
+    public interface IVisitor
+    {
+        void VisitElement(LightElementNode element);
+        void VisitText(LightTextNode text);
+    }
+}
